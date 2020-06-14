bbe04d9fd773ff696eb442930318d6d2fd1f49c6
tom-waka <one.sukt.one@gmail.com>
2020-06-14 11:25:43 +0900

    回答後の挙動を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './assets/styles/style.css';
 import defaultDataset from './dataset';
-import {AnswersList} from './components/index';
+import {AnswersList, Chats} from './components/index';
+
 
 export default class App extends React.Component{
   constructor(props) {
@@ -13,18 +14,47 @@ export default class App extends React.Component{
       dataset: defaultDataset,
       open: false
     }
+    this.selectAnswer = this.selectAnswer.bind(this);
   }
 
-  initAnswer = () => {
-    const initDataset = this.state.dataset[this.state.currentId];
-    const initAnswers = initDataset.answers;
-    this.setState( {
-      answers: initAnswers
+  displayNextQuestion = (nextQuestionId) => {
+    const chats = this.state.chats;
+    chats.push({
+      text: this.state.dataset[nextQuestionId].question,
+      type: 'question'
+    })
+
+    this.setState({
+      answers: this.state.dataset[nextQuestionId].answers,
+      chats: chats,
+      currentId: nextQuestionId
     })
   }
 
+  selectAnswer = (selectedAnswer, nextQuestionId) => {
+    switch(true) {
+      case (nextQuestionId === 'init'):
+        this.displayNextQuestion(nextQuestionId);
+        break;
+      default:
+        const chats = this.state.chats;
+        chats.push({
+          text: selectedAnswer,
+          type: 'answer'
+        })
+    
+        this.setState( {
+          chats: chats
+        })
+
+        this.displayNextQuestion(nextQuestionId);
+        break;
+    }
+  }
+
   componentDidMount() {
-    this.initAnswer();
+    const initAnswer = "";
+    this.selectAnswer(initAnswer, this.state.currentId);
   }
 
   render() {
@@ -32,7 +62,12 @@ export default class App extends React.Component{
       <div>
         <section className="c-section">
           <div className="c-box">
-            <AnswersList answers={this.state.answers}/>
+            <div className="chats_area">
+              <Chats chats={this.state.chats}/>
+            </div>
+            <div className="answers_area">
+              <AnswersList answers={this.state.answers} select={this.selectAnswer}/>
+            </div>
           </div>
         </section>
         
--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
 const Answer = (props) => {
   // const classes = useStyles();
   return(
-    <Button variant="contained">
+    <Button variant="contained" onClick={() => props.select(props.content, props.nextId)}>
       {props.content}
     </Button>
   )
