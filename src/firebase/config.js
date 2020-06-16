d3d7dfc78d077b7b6709447388d09ea288515aac
tom-waka <one.sukt.one@gmail.com>
2020-06-16 22:19:27 +0900

    firestoreからデフォルトのdatasetを取得

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBpgeEVaAfOYo-ktlyd6Sjfyu--Q_AUNs",
+  authDomain: "refuse-game.firebaseapp.com",
+  databaseURL: "https://refuse-game.firebaseio.com",
+  projectId: "refuse-game",
+  storageBucket: "refuse-game.appspot.com",
+  messagingSenderId: "170961599545",
+  appId: "1:170961599545:web:5b346ed68c7d72918121e8",
+  measurementId: "G-NM2FNX3LW3"
+};
+
+export default firebaseConfig;
